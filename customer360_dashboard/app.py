148303613d88a60bc807b632c292630843050b82
rajaswethaa <earlaswetha93@gmail.com>
2026-03-05 20:44:52 +0530

    Delete customer360_dashboard directory

--- customer360_dashboard/app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Customer 360 Dashboard", layout="wide")
-
-st.title("📊 Customer 360 Executive Dashboard")
-st.markdown("Snowflake-powered customer analytics")
